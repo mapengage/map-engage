a67de2034a623bbc222f7f5983e0aa52c1926beb
Varun G <varungadde@outlook.com>
2024-10-26 16:36:34 -0400

    Lat and Lng co-ordinates added to scrape-events.py and events.json

--- scrape-events.py
+++ scrape-events.py
@@ -4,11 +4,12 @@ import re
 import json
 
 class NinerEvent:
-    def __init__(self, name, start, end, geo, url, location, description):
+    def __init__(self, name, start, end, lat, lng, url, location, description):
         self.name = name
         self.start = start
         self.end = end
-        self.geo = geo
+        self.lat = lat
+        self.lng = lng
         self.location = location
         self.description = description
         self.url = url 
@@ -27,14 +28,17 @@ def get_ics_data(ics_url='https://ninerengage.charlotte.edu/events.ics', print_n
         pal = "PAL Session"
         # Checking if the event is a PAL Session or It is cancelled 
         if pal != event.name[:11] and event.status != "CANCELLED":
-            geo = ""
+            lat = ""
+            lng = ""
             if event.geo:
-                geo = f"{event.geo[0]},{event.geo[1]}"
+                lat = f"{event.geo[0]}"
+                lng = f"{event.geo[1]}"
             Niner_Event = NinerEvent(
                 name = event.name,
                 start = f"{event.begin}",
                 end = f"{event.end}",
-                geo = geo,
+                lat = lat,
+                lng = lng,
                 url = event.url,
                 location = event.location,
                 description = event.description)
@@ -44,21 +48,22 @@ def get_ics_data(ics_url='https://ninerengage.charlotte.edu/events.ics', print_n
         print(f"Num of Events: {len(event_list)} ")
         counter = 0
         for event in event_list:
-            if event.geo != "": 
+            if event.lat != "" and event.lng != "": 
                 counter += 1
 
         print(f"Num of Events with Geo: {counter} ")
 
     return event_list
 
-def jsonify_events(event_list, file_path="./events.json"):
+def jsonify_events(event_list, file_path="./map-engage/src/data/events.json"):
     event_list_json = []
     for event in event_list:
         event_dict = {
             "name": event.name,
             "start": event.start,
             "end": event.end,
-            "geo": event.geo,
+            "lat": event.lat,
+            "lng": event.lng,
             "url": event.url,
             "location": event.location,
             "description": event.description}
@@ -66,6 +71,8 @@ def jsonify_events(event_list, file_path="./events.json"):
         with open(file_path, "w") as f: 
             json.dump(event_list_json, f)
 
-event_list = get_ics_data()
-jsonify_events(event_list)
+def make_events():
+    event_list = get_ics_data()
+    jsonify_events(event_list)
     
+make_events()
